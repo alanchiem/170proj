f32bc40b0712fa8070067c5a4c6b93c932c7db3f
alanchiem <chiemalan@gmail.com>
2022-04-25 16:52:50 -0700

    added spiral

--- project-sp22-skeleton-main/python/solve.py
+++ project-sp22-skeleton-main/python/solve.py
@@ -25,23 +25,40 @@ def solve_naive(instance: Instance) -> Solution:
         towers=instance.cities,
     )
 
-
+# Description
+# 1. Loop through all possible tower positions. Find the tower that covers as many cities as possible.
+# 2. Add tower to towerslist and remove the cities it covered.
+    # Optimziation: remove that tower position from possible tower positions since we won't place two towers in the same place
+# 3. Repeat until all cities are covered.
 def solve_set_cover(instance: Instance) -> Solution:
     gsl = instance.grid_side_length
     cr = instance.coverage_radius
-    pr = instance.penalty_radius # ignored
+    pr = instance.penalty_radius
     citiesList = instance.cities
     towersList = []
 
-    # Description
-    # 1. Loop through all possible tower positions. Find the tower that covers as many cities as possible.
-    # 2. Add tower to towerslist and remove the cities it covered.
-        # Optimziation: remove that tower position from possible tower positions since we won't place two towers in the same place
-    # 3. Repeat until all cities are covered.
-
     possibleTowerPositions = []
-    for x in range(0, gsl):
-        for y in range(0, gsl):
+    # original
+    # for x in range(0, gsl):
+    #     for y in range(0, gsl):
+    #         possibleTowerPositions.append(Point(x,y))
+
+    # spiral
+    layers = pr
+    currLayer = 0
+    while currLayer != layers:
+        for x in range(currLayer, gsl - currLayer - 1):
+            possibleTowerPositions.append(Point(x, currLayer))
+        for y in range(currLayer, gsl - currLayer - 1):
+            possibleTowerPositions.append(Point(gsl - currLayer - 1, y))
+        for x in range(gsl - currLayer - 1, currLayer, -1):
+            possibleTowerPositions.append(Point(x, gsl - currLayer - 1))
+        for y in range(gsl - currLayer - 1, currLayer, - 1):
+            possibleTowerPositions.append(Point(currLayer, y))
+        currLayer += 1
+
+    for x in range(layers, gsl - layers):
+        for y in range(layers, gsl - layers):
             possibleTowerPositions.append(Point(x,y))
 
     def numCitiesCovered(point):
@@ -64,7 +81,7 @@ def solve_set_cover(instance: Instance) -> Solution:
         coveredByMax = []
         for tower in possibleTowerPositions:
             coverageCount = numCitiesCovered(tower)
-            coveredList = getCitiesCovered(tower)
+            coveredList = getCitiesCovered(tower) # list of cities that the tower covers
             if coverageCount > max_coverage:
                 max_tower = tower
                 max_coverage = coverageCount
@@ -74,22 +91,26 @@ def solve_set_cover(instance: Instance) -> Solution:
         citiesList = list(set(citiesList) - set(coveredByMax))
 
     return Solution(
-        instance=instance,
-        towers=towersList,
-    )
-    
-
-
-def solve_dp(instance: Instance) -> Solution:
-
-    towersList = instance.cities + [Point(28,29)]
-
-    return Solution(
         instance = instance,
         towers = towersList
     )
 
+# https://stackoverflow.com/questions/127704/algorithm-to-return-all-combinations-of-k-elements-from-n
+# Was gonna use this to brute force but it'd probably take waayyy to long
+def combinations(list, length, startPosition, result, combinationList):
+    if length == 0:
+        combinationList.append(result)
+        return
+    for i in range(startPosition, (len(list) - length) + 1 ):
+        result[len(result) - length] = list[i]
+        combinations(list, length - 1, i + 1, result, combinationList)
 
+
+def solve_dp(instance: Instance) -> Solution:
+    return Solution(
+        instance = instance,
+        towers = towersList
+    )
 
 SOLVERS: Dict[str, Callable[[Instance], Solution]] = {
     "naive": solve_naive,
